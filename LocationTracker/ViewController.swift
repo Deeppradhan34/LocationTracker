231a3e0835135a5cd577ed4d51e63782e804c265
DeepPradhan <deeppradhan34@gmail.com>
2018-11-15 17:01:03 +0530

    Initial Commit

---
+++ LocationTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationTracker
+//
+//  Created by Deep on 15/11/18.
+//  Copyright © 2018 Deep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
